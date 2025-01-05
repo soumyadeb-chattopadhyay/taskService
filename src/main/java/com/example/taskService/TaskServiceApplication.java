a4303c9276587018727c538ac354704938e4f51e
sochatto <soumyadeb.chattopadhyay@oracle.com>
2025-01-05 20:14:46 +0530

    test commit

---
+++ src/main/java/com/example/taskService/TaskServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.taskService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//Hello
+public class TaskServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskServiceApplication.class, args);
+	}
+
+}
