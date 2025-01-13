bc5026eb9e22b8bc5fb85516c1368cad66bbfdaa
sochatto <soumyadeb.chattopadhyay@oracle.com>
2025-01-13 16:25:13 +0530

    restrict tasks with same groupid to execute concurrently

--- src/main/java/com/example/taskService/TaskServiceApplication.java
+++ src/main/java/com/example/taskService/TaskServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.taskService;
 
 import java.util.Random;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -32,10 +34,20 @@ public class TaskServiceApplication {
 		
 		int  i = 0;
 		while(i < TASK_LIST_CAPACITY) {
-			Main.TaskGroup taskGoup = new TaskGroup(UUID.randomUUID());
+			UUID uuid = UUID.fromString("84c3d943-a255-4302-8a19-5992726bf2f7");
+			Main.TaskGroup taskGoup = new TaskGroup(uuid);
 			String callableReturnString = "Task executed "+i+" ";
 			Main.Task<String> taskInstance = new Main.Task<String>(UUID.randomUUID(),taskGoup, generateTaskType(), () -> {
+				 ConcurrentHashMap<UUID, ReentrantLock> locks = new ConcurrentHashMap<>();
+				 ReentrantLock lock = locks.computeIfAbsent(taskGoup.groupUUID(), k -> new ReentrantLock()); 
+			        lock.lock(); 
+			        try {
 			    TimeUnit.MILLISECONDS.sleep(CALLABLE_EXECUTION_DURATION_MS);
+			        }catch(InterruptedException e) {
+			            Thread.currentThread().interrupt();
+			        } finally {
+			            lock.unlock();
+			        }
 			    return callableReturnString.concat("task groudId "+taskGoup.groupUUID());
 			}) ;
 			taskList.add(taskInstance);
